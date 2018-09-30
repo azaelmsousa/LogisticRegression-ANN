2aec1821bd65e5cc9ff2ea710a9f77dab576c040
Waner Miranda <wanermiranda@gmail.com>
2018-09-29 22:45:15 -0300

    Added a test with the same outputs from the class slides with an assert to check the values

--- NN/network.py
+++ NN/network.py
@@ -16,27 +16,26 @@ from sklearn.preprocessing import LabelBinarizer
 from NN import activation_functions, loss_functions
 
 
-
 class Layer:
-    def __init__(self, input_sz, neurons, activation, weights=None, label=None):
-        self.activation = activation_functions.__dict__[activation]        
+    def __init__(self, input_sz, neurons, activation, bias=1.0, weights=None, label=None):
+        self.activation = activation_functions.__dict__[activation]
         self.input_sz = input_sz
         self.neurons = neurons
         self.label = label
-        
+        self.bias = bias
         if activation != 'softmax':
             self.act_derivative = activation_functions.__dict__[
                 activation+"_derivative"]
             if weights is not None:
                 self.weights = weights
             else:
-                self.weights = np.random.rand(input_sz+1, neurons)
-        else:             
-            self.weights = np.ones((input_sz+1, neurons))
+                self.weights = np.random.rand(input_sz, neurons)
+        else:
+            self.weights = np.ones((input_sz, neurons))
 
     def __str__(self):
         return inspect.cleandoc("""
-        {}\t(input={}, neurons={}, activation={})""".format(self.label,self.input_sz,self.neurons, self.activation.__name__))
+        {}\t(input={}, neurons={}, activation={})""".format(self.label, self.input_sz, self.neurons, self.activation.__name__))
 
     def get_weights(self):
         return self.weights
@@ -44,22 +43,25 @@ class Layer:
     def set_weights(self, weights):
         self.weights = weights
 
-
     def feed_forward(self, input_values):
-        print("Forwardign {} weights {} {}".format(self.label, input_values.shape, self.weights.shape))
 
-        if self.activation.__name__ != 'softmax':        
+        if self.activation.__name__ != 'softmax':
             _input_values = input_values.copy()
-            _input_values = np.insert(_input_values, 0, 1, axis=1)
-            output_values = np.matmul(_input_values, self.get_weights())
-        else: 
-            output_values = input_values
 
-        activation_value = self.activation(output_values)
-        
-        return output_values, activation_value
+            W = self.get_weights().copy()
+            B = np.ones(input_values.shape[0])*self.bias
+            print(self.label, _input_values.shape, W.shape)
+            net = np.matmul(_input_values, W) + B
+        else:
+            net = input_values.copy()
 
-class NN:    
+        out = self.activation(net)
+        print('net', net)
+        print('out', out)
+        return net, out
+
+
+class NN:
     def __init__(self, loss):
         self.clear_layers()
         self.loss = loss_functions.__dict__[loss]
@@ -68,58 +70,39 @@ class NN:
     def clear_layers(self):
         self.layers = []
 
-    def add_layer(self, layer):        
+    def add_layer(self, layer):
         self.layers.append(layer)
 
     def summary(self):
         print("Model Summary")
         print("-------------------------------")
-        for layer in self.layers: 
-            print (layer)
+        for layer in self.layers:
+            print(layer)
         print("-------------------------------")
 
     def show_weights(self):
         print("Model Weights")
         print("-------------------------------")
-        for layer in self.layers: 
-            print (layer)            
-            print (layer.get_weights())         
-        print("-------------------------------")               
-    
+        for layer in self.layers:
+            print(layer)
+            print(layer.get_weights())
+        print("-------------------------------")
+
     def feed_forward(self, X):
-        input_values = X        
-        for layer in self.layers[:-1]:            
-            output_values, activation_value = layer.feed_forward(input_values)            
-            input_values = activation_value
+        input_values = X
+        for layer in self.layers:
+            net, out = layer.feed_forward(input_values)
+            input_values = out
 
-        output_values, activation_value = self.layers[-1].feed_forward(input_values)            
-        return output_values, activation_value
+        return net, out
 
-    def back_propagante(error): 
+    def back_propagante(error):
         return None
 
-    def fit(self,X,y, max_it=100):
+    def fit(self, X, y, max_it=100):
         it = 0
         while it < max_it:
             _, pdf = self.feed_forward(X)
-            y_  = pdf.argmax(axis=-1)
+            y_ = pdf.argmax(axis=-1)
 
             it += 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
